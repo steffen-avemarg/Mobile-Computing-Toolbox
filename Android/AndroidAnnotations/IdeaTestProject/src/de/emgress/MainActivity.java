9bbe6f7bb0dcf8103e25c6a1fcf8c9ee543d37c8
Steffen Kern <kern@jena>
2012-06-08 12:27:40 +0200

    Added the first Version of an IntelliJ Idea Android Project that is configured for the usage of android annotations (androidannotations.org)

---
+++ Android/AndroidAnnotations/IdeaTestProject/src/de/emgress/MainActivity.java
@@ -0,0 +1,28 @@
+package de.emgress;
+
+import android.app.Activity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import com.googlecode.androidannotations.annotations.Click;
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.main)
+public class MainActivity extends Activity
+{
+	public static final String LOG_TAG = "AA MainActivity";
+
+	@ViewById( R.id.mytextview )
+	TextView textview;
+
+	@Click( R.id.button )
+	public void buttonPressed( View pressedButton )
+	{
+		Log.i( LOG_TAG, "Button Pressed - Object: " + pressedButton.toString() );
+
+		textview.setText( "Awesome!" );
+	}
+
+
+}
