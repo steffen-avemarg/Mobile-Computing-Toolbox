26186fb06d7815ecae68e669fc004fcc4ac9a9b3
Steffen Kern <kern@jena>
2012-06-26 11:58:35 +0200

    Added Options Menu to add and remove Groups to the Dataset

--- Android/ExpandableListAdapterExample/gen/de/emgress/android/expandableadapter/R.java
+++ Android/ExpandableListAdapterExample/gen/de/emgress/android/expandableadapter/R.java
@@ -14,18 +14,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int child_item=0x7f050000;
-        public static final int child_name=0x7f050001;
-        public static final int child_text=0x7f050002;
-        public static final int exp_list_view=0x7f050006;
-        public static final int group_item=0x7f050003;
-        public static final int group_name=0x7f050004;
-        public static final int group_size=0x7f050005;
+        public static final int add_item=0x7f060007;
+        public static final int child_item=0x7f060000;
+        public static final int child_name=0x7f060001;
+        public static final int child_text=0x7f060002;
+        public static final int exp_list_view=0x7f060006;
+        public static final int group_item=0x7f060003;
+        public static final int group_name=0x7f060004;
+        public static final int group_size=0x7f060005;
+        public static final int remove_item=0x7f060008;
     }
     public static final class layout {
         public static final int child_item_layout=0x7f030000;
         public static final int group_item_layout=0x7f030001;
         public static final int main=0x7f030002;
+    }
+    public static final class menu {
+        public static final int option_menu=0x7f050000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
--- Android/ExpandableListAdapterExample/src/de/emgress/android/expandableadapter/MainActivity.java
+++ Android/ExpandableListAdapterExample/src/de/emgress/android/expandableadapter/MainActivity.java
@@ -4,10 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.*;
 import android.widget.AbsListView;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.ExpandableListView;
@@ -21,6 +18,8 @@ public class MainActivity extends Activity
     private static final String LOG_TAG = "ELA_DEMO";
 
     private List<GroupClass> groups;
+    private ExpandableListView elv;
+    private MyExpandableListAdapter adapter;
 
 
     @Override
@@ -39,26 +38,132 @@ public class MainActivity extends Activity
         /*
          * Create Adapter and configure ListView
          */
-        MyExpandableListAdapter adapter = new MyExpandableListAdapter();
+        this.adapter = new MyExpandableListAdapter();
 
-        ExpandableListView elv = (ExpandableListView)findViewById( R.id.exp_list_view );
-        elv.setAdapter( adapter );
+        this.elv = (ExpandableListView)findViewById( R.id.exp_list_view );
+        this.elv.setAdapter( this.adapter );
     }
 
+    /*
+     * Option Menu related classes.
+     * We provide two menu items - one to add a new group with one child element
+     * and a second one which deletes the first group in the list.
+     * In both cases, we notify our adapter that the dataset changed so that the
+     * ListView will be updated to reflect those changes.
+     */
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater mi = new MenuInflater( this );
+        mi.inflate( R.menu.option_menu, menu );
+
+        return true;
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item)
+    {
+        if( item.getItemId() == R.id.add_item )
+        {
+            GroupClass newGroup = new GroupClass( "New Group " + System.currentTimeMillis() );
+            newGroup.addChild( new ChildClass( "New Child", "Child Text: " + System.currentTimeMillis() ) );
+            this.groups.add( 0, newGroup );
+
+            this.adapter.notifyDataSetChanged();
+
+            return true;
+        }
+        else if ( item.getItemId() == R.id.remove_item )
+        {
+            this.groups.remove( 0 );
+
+            this.adapter.notifyDataSetChanged();
+
+            return true;
+        }
+        else
+        {
+            return super.onMenuItemSelected(featureId, item);
+        }
+    }
+
+    /**
+     * The adapter uses the group List from the surrounding class which is sufficient
+     * for the example. However, to achieve a higher encapsulation, one would probably
+     * hide the data structure in a separate object and not expose it to the Activity.
+     */
     public class MyExpandableListAdapter extends BaseExpandableListAdapter
     {
+
+        // Local Reference to the LayoutInflater because we will need it
+        // several times
         LayoutInflater inflater = (LayoutInflater)MainActivity.this.getSystemService
                 (Context.LAYOUT_INFLATER_SERVICE);
+
+
+        /*
+         * Group related Methods
+         */
+
+        @Override
+        public Object getGroup( int groupPosition )
+        {
+            return groups.get( groupPosition );
+        }
+
+        @Override
+        public int getGroupCount()
+        {
+            return groups.size();
+        }
+
+        @Override
+        public long getGroupId( int groupPosition )
+        {
+            return groupPosition;
+        }
+
+        @Override
+        public View getGroupView( int groupPosition, boolean isExpanded, View convertView,
+                                  ViewGroup parent )
+        {
+            ViewGroup item;
+
+            if( convertView != null && convertView.getId() == R.id.group_item )
+            {
+                // We can reuse the View
+                item = (ViewGroup)convertView;
+            }
+            else
+            {
+                // We create a new View
+                item = (ViewGroup)inflater.inflate( R.layout.group_item_layout, parent, false );
+            }
+
+            GroupClass group = (GroupClass)getGroup(groupPosition);
+
+            TextView nameView = (TextView)item.findViewById( R.id.group_name );
+            nameView.setText( group.getName() );
+
+            TextView sizeView = (TextView)item.findViewById( R.id.group_size );
+            sizeView.setText( "" + group.getChildren().size() );
+
+            return item;
+        }
+
+        /*
+         * Child related Methods
+         */
 
         @Override
         public Object getChild( int groupPosition, int childPosition )
         {
-            return groups.get( groupPosition ).getChild(childPosition);
+            return groups.get( groupPosition ).getChild( childPosition );
         }
 
         @Override
-        public long getChildId(int groupPosition, int childPosition)
+        public long getChildId( int groupPosition, int childPosition )
         {
             return childPosition;
         }
@@ -82,6 +187,7 @@ public class MainActivity extends Activity
             }
             else
             {
+                // We create a new View
                 item = (ViewGroup)inflater.inflate( R.layout.child_item_layout, parent, false );
             }
 
@@ -98,51 +204,6 @@ public class MainActivity extends Activity
         }
 
         @Override
-        public Object getGroup( int groupPosition )
-        {
-            return groups.get( groupPosition );
-        }
-
-        @Override
-        public int getGroupCount()
-        {
-            return groups.size();
-        }
-
-        @Override
-        public long getGroupId( int groupPosition )
-        {
-            return groupPosition;
-        }
-
-        @Override
-        public View getGroupView( int groupPosition, boolean isExpanded, View convertView,
-                                 ViewGroup parent )
-        {
-            ViewGroup item;
-
-            if( convertView != null && convertView.getId() == R.id.group_item )
-            {
-                // We can reuse the view
-                item = (ViewGroup)convertView;
-            }
-            else
-            {
-                item = (ViewGroup)inflater.inflate( R.layout.group_item_layout, parent, false );
-            }
-
-            GroupClass group = (GroupClass)getGroup(groupPosition);
-
-            TextView nameView = (TextView)item.findViewById( R.id.group_name );
-            nameView.setText( group.getName() );
-
-            TextView sizeView = (TextView)item.findViewById( R.id.group_size );
-            sizeView.setText( "" + group.getChildren().size() );
-
-            return item;
-        }
-
-        @Override
         public boolean isChildSelectable(int groupPosition, int childPosition)
         {
             return true;
@@ -153,17 +214,16 @@ public class MainActivity extends Activity
         {
             return true;
         }
-
     }
 
     /**
-     * Helper Method to create Demo Groups and Childen
+     * Helper Method to create Demo Groups and Children
      */
     private void initDemoData()
     {
         this.groups = new ArrayList<GroupClass>();
 
-        for( int groupCounter = 1; groupCounter <= 20; groupCounter++ )
+        for( int groupCounter = 1; groupCounter <= 25; groupCounter++ )
         {
             GroupClass group = new GroupClass( "Gruppe " + groupCounter );
 
